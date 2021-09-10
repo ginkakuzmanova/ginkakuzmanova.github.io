b251e5695f0616ecb31e9d447efb47bcf7352617
GinkaKuzmanov <ginkakuzmanova@gmail.com>
2021-09-10 22:51:47 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+const navMenu = document.getElementById('nav-menu'),
+  toggleMenu = document.getElementById('nav-toggle'),
+  closeMenu = document.getElementById('nav-close')
+
+toggleMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('show')
+})
+
+closeMenu.addEventListener('click', () => {
+  navMenu.classList.remove('show')
+})
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+document.querySelectorAll('.nav__link').forEach(item =>
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('.active').classList.remove('active');
+
+    if (e.target.classList.contains('nav__link')) {
+      const id = e.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    }
+    e.target.classList.add('active');
+  }))
